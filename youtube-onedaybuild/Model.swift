bed5ccbe0c3f9e1fc08899f902bb24a38dd867ef
oscarli9 <guowel2@uci.edu>
2020-07-20 15:29:42 -0700

    Added code to parse the JSON response

--- youtube-onedaybuild/Model.swift
+++ youtube-onedaybuild/Model.swift
@@ -30,7 +30,21 @@ class Model {
                 return
             }
             
-            //Parsinng the data into video objects
+            do {
+                
+                //Parsing the data into video objects
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                
+                let response = try decoder.decode(Response.self, from: data!)
+                
+                dump(response)
+                
+            } catch {
+                
+            }
+            
+            
         }
         
         // Kick off the task
